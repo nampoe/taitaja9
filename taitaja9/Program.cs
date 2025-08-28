b4b98f5f41b83040b18bfe0da4029d458e1f8118
katisaara <katike.kemppainen@gmail.com>
2025-08-28 12:18:26 +0300

    Update Program.cs

--- taitaja9/Program.cs
+++ taitaja9/Program.cs
@@ -25,6 +25,33 @@ namespace taitaja9
             Console.Clear(); // Päivittää koko konsolin taustavärin
 
             Console.WriteLine($"Nimi: {nimi}, Laji: {laji}, Pisteet: {pisteet}");   //konsoli tulostaa nimen, lajin ja pisteet ruudulle.   Sitten ohjelma siirtää tiedot yamliin
+
+            static void HaeTuloksia()
+            {
+                Console.Write("Anna nimi haettavaksi: ");
+                string haku = Console.ReadLine().ToLower();
+
+                var loydetyt = tulokset.FindAll(t => t.Nimi.ToLower().Contains(haku));
+
+                if (loydetyt.Count > 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("\n{0,-20} {1,-15} {2,-10}", "Nimi", "Laji", "Tulos");
+                    Console.WriteLine(new string('-', 50));
+                    Console.ResetColor();
+
+                    foreach (var t in loydetyt)
+                    {
+                        Console.WriteLine("{0,-20} {1,-15} {2,-10}", t.Nimi, t.Laji, t.TulosArvo);
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ei tuloksia annetulla nimellä.");
+                    Console.ResetColor();
+                }
+            }
         }
     }
 }
