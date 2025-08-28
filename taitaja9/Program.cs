40a10f5f9dcc1277285b8904285ffd316e1d9df0
katisaara <katike.kemppainen@gmail.com>
2025-08-28 11:05:52 +0300

    Update Program.cs

--- taitaja9/Program.cs
+++ taitaja9/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+namespace taitaja9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna etu- ja sukunimi");         // kysytään käyttäjältä etu- ja sukunimi hakua ja suodatusta varten
+            string nimi = Console.ReadLine();
+
+            Console.Write("Anna laji");                     //kysytään käyttäjältä laji hakemista ja suodatusta varten
+            string laji = Console.ReadLine();
+
+            Console.Write("Anna pisteet");                     //kysytään käyttäjältä pisteet suodatusta varten
+            string pisteetSyote = Console.ReadLine();
+            int pisteet = int.Parse(pisteetSyote);
+
+            Console.WriteLine($"Nimi: {nimi}, Laji: {laji}, Pisteet: {pisteet}");   //konsoli tulostaa nimen, lajin ja pisteet ruudulle.    Sitten ohjelma siirtää tiedot yamliin
+
+
+
+
+        }
+    }
+}
