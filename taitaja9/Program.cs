b09c20478c9530db64c61484b62b4926445c1967
nampoe <75635660+nampoe@users.noreply.github.com>
2025-08-28 10:47:19 +0300

    Add files via upload

---
+++ taitaja9/Program.cs
@@ -0,0 +1,16 @@
+﻿using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using System.IO;
+
+string filePath = "data.yaml";
+
+var deserializer = new DeserializerBuilder()
+    .WithNamingConvention(CamelCaseNamingConvention.Instance) 
+    .Build();
+public class Person
+{
+    public string Name {  get; set; }
+    public string Laji { get; set; }
+    public int Tulos { get; set; }
+}
+
